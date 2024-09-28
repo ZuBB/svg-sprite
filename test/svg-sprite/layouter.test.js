3c83974882ba6d36e9ebccd713b693436ddaea88
Vasyl Zuziak <zvasylvv@gmail.com>
2024-09-28 20:49:57 +0200

    refactor: convert 'SVGSpriteSymbol' module to ESM

--- test/svg-sprite/layouter.test.js
+++ test/svg-sprite/layouter.test.js
@@ -4,14 +4,14 @@ import { SVGSpriteLayouter } from '../../lib/svg-sprite/layouter.mjs';
 import CSSLayouter from '../../lib/svg-sprite/mode/css.mjs';
 import DefsLayouter from '../../lib/svg-sprite/mode/defs.mjs';
 import StackLayouter from '../../lib/svg-sprite/mode/stack.mjs';
+import SymbolLayouter from '../../lib/svg-sprite/mode/symbol.mjs';
 const SVGSpriter = require('../../lib/svg-sprite.js');
-const SymbolLayouter = require('../../lib/svg-sprite/mode/symbol.js');
 const ViewLayouter = require('../../lib/svg-sprite/mode/view.js');
 
 jest.mock('../../lib/svg-sprite/mode/css.mjs');
 jest.mock('../../lib/svg-sprite/mode/defs.mjs');
 jest.mock('../../lib/svg-sprite/mode/stack.mjs');
-jest.mock('../../lib/svg-sprite/mode/symbol.js');
+jest.mock('../../lib/svg-sprite/mode/symbol.mjs');
 jest.mock('../../lib/svg-sprite/mode/view.js');
 
 describe('testing layout()', () => {
