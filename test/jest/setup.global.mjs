531cf8548de5cf96b69273cdb39ae05516cdded7
Vasyl Zuziak <zvasylvv@gmail.com>
2024-09-30 21:05:22 +0200

    jest/setup.global

--- test/jest/setup.global.mjs
+++ test/jest/setup.global.mjs
@@ -1,7 +1,5 @@
-'use strict';
-
 import removeTmpPath from '../helpers/remove-temp-path.mjs';
 
-module.exports = async() => {
+export default async function globalSetup() {
   await removeTmpPath();
-};
+}
